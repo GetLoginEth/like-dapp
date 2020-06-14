9769926b8b7a9c5246c9a2d677502bf370557d39
Igor Shadurin <igor.shadurin@gmail.com>
2020-06-14 18:08:20 +0300

    display basic info about created resource

--- src/resource/ResourceView.js
+++ src/resource/ResourceView.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, {Fragment, useContext} from 'react';
 import './Resource.css';
 import {useRouteMatch} from "react-router-dom";
+import {ContextApp} from "../reducer/reducer";
+import Spinner from "../Spinner";
 
 function ResourceView() {
     const match = useRouteMatch();
+    const {state: {resources}} = useContext(ContextApp);
+    const id = Number(match.params.id);
+    const resource = resources.find(item => Number(item.id) === id);
 
     return (
         <div className="ResourceView">
-            <header className="App-header">
-                <p>
-                    Resource view id {match.params.id}
-                </p>
+            {!resource && <Spinner/>}
 
-            </header>
+            {resource && <Fragment>
+                <p>
+                    Resource view id {id}
+                </p>
+                <p>{resource.title}</p>
+                <p>{resource.url}</p>
+                <p>{resource.description}</p>
+            </Fragment>}
         </div>
     );
 }
