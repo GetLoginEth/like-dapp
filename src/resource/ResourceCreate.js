c262315acaefce1ac3e68228b5f9abdc34da0d7c
Igor Shadurin <igor.shadurin@gmail.com>
2020-06-16 12:46:16 +0300

    implemented view and edit resource

--- src/resource/ResourceCreate.js
+++ src/resource/ResourceCreate.js
@@ -1,7 +1,6 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext} from 'react';
 import './Resource.css';
 import {actionCreateResource} from "../reducer/actions";
-import WaitButton from "../WaitButton";
 import {ContextApp} from "../reducer/reducer";
 import ResourceForm from "./ResourceForm";
 
@@ -9,17 +8,13 @@ function ResourceCreate() {
     const {state: {inProcess}} = useContext(ContextApp);
     const {state: {user: {usernameHash}}} = useContext(ContextApp);
 
-    /* const [title, setTitle] = useState('');
-     const [url, setUrl] = useState('');
-     const [description, setDescription] = useState('');*/
-
     return (
         <div className="ResourceCreate">
             <h6 className="mb-4">
                 A resource is your site for which you want to collect advanced statistics on likes. You can view
                 statistics for each page and for the whole site as a whole.
             </h6>
-            <ResourceForm inProcess={inProcess} onSubmit={async ({id, title, url, description}) => {
+            <ResourceForm inProcess={inProcess} onSubmit={async ({title, url, description}) => {
                 const result = await actionCreateResource(usernameHash, title, url, description)
                 if (result) {
                     return 'reset';
--- src/resource/ResourceView.js
+++ src/resource/ResourceView.js
@@ -1,14 +1,17 @@
-import React, {Fragment, useContext} from 'react';
+import React, {useContext} from 'react';
 import './Resource.css';
 import {useRouteMatch} from "react-router-dom";
 import {ContextApp} from "../reducer/reducer";
 import Spinner from "../Spinner";
-import {actionCreateResource} from "../reducer/actions";
+import {actionEditResource} from "../reducer/actions";
 import ResourceForm from "./ResourceForm";
 
 export default function ResourceView() {
     const match = useRouteMatch();
+    const {state: {inProcess}} = useContext(ContextApp);
     const {state: {resources}} = useContext(ContextApp);
+    const {state: {user: {usernameHash}}} = useContext(ContextApp);
+
     const id = Number(match.params.id);
     const resource = resources.find(item => Number(item.id) === id);
 
@@ -23,12 +26,9 @@ export default function ResourceView() {
                 url={resource.url}
                 description={resource.description}
                 readOnly={true}
+                inProcess={inProcess}
                 onSubmit={async ({id, title, url, description}) => {
-                    alert('save here');
-                    /*const result = await actionCreateResource(usernameHash, title, url, description)
-                    if (result) {
-                        return 'reset';
-                    }*/
+                    await actionEditResource(usernameHash, id, title, url, description)
                 }}/>}
         </div>
     );
