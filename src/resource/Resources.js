a58e99b7b16de2d1dfa22249cfa2397a329a1800
Igor Shadurin <igor.shadurin@gmail.com>
2020-07-16 17:14:23 +0300

    upgraded with getlogin url for subdomains

--- src/resource/Resources.js
+++ src/resource/Resources.js
@@ -6,6 +6,7 @@ function Resources({resources, onUpdate, isLoading}) {
     useEffect(_ => {
         onUpdate();
     }, []);
+
     return (
         <div className="Resources">
             <header className="App-header">
